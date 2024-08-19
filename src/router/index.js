de96a3221e5905e051e8b070174cc3172c3198e5
wangjiying <2084151024@qq.com>
2024-08-20 00:12:49 +0800

    ip查询接口不支持ssl 暂时停用

--- src/router/index.js
+++ src/router/index.js
@@ -21,27 +21,28 @@ const router = createRouter({
 router.beforeEach(async (to, from, next) => {
   const themeStore = useThemeStore()
   themeStore.initTheme()
-  if (to.path === '/403') {
-    next()
-  } else {
-    try {
-      // 使用 ipapi.co 服务获取 IP 信息
-      const response = await axios.get('http://ip-api.com/json')
-      const {countryCode} = response.data
-
-      // 检查是否为中国大陆 IP
-      if (countryCode === 'CN') {
-        // 如果是中国大陆 IP，重定向到 /403 页面
-        next('/403')
-      } else {
-        // 如果不是中国大陆 IP，允许访问
-        next()
-      }
-    } catch (error) {
-      console.error('获取 IP 信息失败:', error)
-      // 如果获取 IP 信息失败，为了安全起见，重定向到 /403 页面
-      next('/403')
-    }
-  }
+  // if (to.path === '/403') {
+  //   next()
+  // } else {
+  //   try {
+  //     // 使用 ipapi.co 服务获取 IP 信息
+  //     const response = await axios.get('http://ip-api.com/json')
+  //     const {countryCode} = response.data
+  //
+  //     // 检查是否为中国大陆 IP
+  //     if (countryCode === 'CN') {
+  //       // 如果是中国大陆 IP，重定向到 /403 页面
+  //       next('/403')
+  //     } else {
+  //       // 如果不是中国大陆 IP，允许访问
+  //       next()
+  //     }
+  //   } catch (error) {
+  //     console.error('获取 IP 信息失败:', error)
+  //     // 如果获取 IP 信息失败，为了安全起见，重定向到 /403 页面
+  //     next('/403')
+  //   }
+  // }
+  next()
 })
 export default router
